6451e6acc61fd4a2a40c0743d0ad1b0e95969a19
travis <t3patterson@gmail.com>
2017-09-01 13:31:35 -0500

    cleanup examples

--- js/01-event-anatomy.js
+++ js/01-event-anatomy.js
@@ -9,33 +9,5 @@
 //(3) add the event listener to the element that will receive the event,
 //     « elemenet ».addEventListener( «evt-type» , « event-handler function » )
 
-//alertBtnEl.addEventListener('click',popAlert)
-
-
-// B : using the event-handler function to modify another element
-//--------------------------------------------
-var writeBtn = document.querySelector('.write-btn')
-var writeSomething = function(){
-   // it all happens here...
-   var el = document.querySelector('.top.right')
-   el.innerHTML = '<h3>This appeared from clicking <code>button.write-btn</code> elsewhere</h3>'
-}
-writeBtn.addEventListener('click', writeSomething)
-
-
-
-
-// C : evt.target to *dynamically select* and then modify
-//     the element where the event happened
-//-------------------------------------------------
-var doItBtn = document.querySelector('.do-it-btn')
-var h2El = document.querySelector('.yo-h2')
-
-var modifyClickedElement = function(evt){
-   console.log(evt.target)
-   evt.target.innerHTML = "WOAHHHH CLICKED!"
-   evt.target.classList.add('urgent')
-}
-
-doItBtn.addEventListener('mouseover', modifyClickedElement)
-h2El.addEventListener('click', modifyClickedElement)
+alertBtnEl.addEventListener('click',popAlert)
+alertBtnEl.addEventListener('click',popAlert)
